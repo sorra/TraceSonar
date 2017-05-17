114d6a8addf6079e8cd7b45c21544bbaa7f73b67
Dongqing Hu <sorra@outlook.com>
2017-05-17 11:20:51 +0800

    Handle archives in directories

--- src/main/java/sorra/tracesonar/core/FileWalker.java
+++ src/main/java/sorra/tracesonar/core/FileWalker.java
@@ -17,7 +17,7 @@ import java.util.jar.JarFile;
 import sorra.tracesonar.util.FileUtil;
 
 public class FileWalker {
-  public static void walkAll(Collection<String> files, Collection<String> ignores) {
+  public static void walkAll(Collection<String> roots, Collection<String> ignores) {
     try {
       ExecutorService es = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
       Consumer<InputStream> submitter = in -> es.submit(() -> {
@@ -28,31 +28,22 @@ public class FileWalker {
         }
       });
 
-      for (String file : files) {
-        Path fPath = Paths.get(file);
-        if (fPath.toFile().isDirectory()) {
-          Files.walk(fPath)
-              .filter(path -> path.toString().endsWith(".class") && !path.toFile().isDirectory())
+      for (String root : roots) {
+        Path rootPath = Paths.get(root);
+        if (rootPath.toFile().isDirectory()) {
+          Files.walk(rootPath)
               .forEach(path -> {
-                try {
-                  submitter.accept(new FileInputStream(path.toFile()));
-                } catch (FileNotFoundException e) {
-                  throw new UncheckedIOException(e);
+                String pathStr = path.toString();
+                if (pathStr.endsWith(".class") && !path.toFile().isDirectory()) {
+                  handleClassFile(path, submitter);
+                }
+                if (arSuffixes.stream().anyMatch(pathStr::endsWith)) {
+                  handleJarFile(path, submitter);
                 }
               });
         } else {
-          if (arSuffixes.stream().anyMatch(file::endsWith)) {
-            JarFile jarFile = new JarFile(fPath.toFile());
-            Enumeration<JarEntry> entries = jarFile.entries();
-            while (entries.hasMoreElements()) {
-              JarEntry entry = entries.nextElement();
-              if (entry.getName().endsWith(".class") && !entry.isDirectory()) {
-                try (InputStream classIn = jarFile.getInputStream(entry)) {
-                  byte[] classBytes = FileUtil.read(classIn, 1024);
-                  submitter.accept(new ByteArrayInputStream(classBytes));
-                }
-              }
-            }
+          if (arSuffixes.stream().anyMatch(root::endsWith)) {
+            handleJarFile(rootPath, submitter);
           }
         }
       }
@@ -60,6 +51,32 @@ public class FileWalker {
       es.awaitTermination(10, TimeUnit.MINUTES);
     } catch (IOException | InterruptedException e) {
       throw new RuntimeException(e);
+    }
+  }
+
+  private static void handleJarFile(Path path, Consumer<InputStream> submitter) {
+    try {
+      JarFile jarFile = new JarFile(path.toFile());
+      Enumeration<JarEntry> entries = jarFile.entries();
+      while (entries.hasMoreElements()) {
+        JarEntry entry = entries.nextElement();
+        if (entry.getName().endsWith(".class") && !entry.isDirectory()) {
+          try (InputStream classIn = jarFile.getInputStream(entry)) {
+            byte[] classBytes = FileUtil.read(classIn, 1024);
+            submitter.accept(new ByteArrayInputStream(classBytes));
+          }
+        }
+      }
+    } catch (IOException e) {
+      throw new UncheckedIOException(e);
+    }
+  }
+
+  private static void handleClassFile(Path path, Consumer<InputStream> submitter) {
+    try {
+      submitter.accept(new FileInputStream(path.toFile()));
+    } catch (FileNotFoundException e) {
+      throw new UncheckedIOException(e);
     }
   }
 
